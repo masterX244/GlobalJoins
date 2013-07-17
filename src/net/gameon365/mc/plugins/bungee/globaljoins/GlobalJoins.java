bb387b44360667800edac34c08b92ca61202c998
NickG365 <nick@nickxp.com>
2013-07-17 07:01:02 -0400

    Changed events that triggered join/leave messages
    No longer displays messages for players who are kicked while joining the
    default server.  This fixes showing join/leave messages for players who
    are banned on the default server, when a Bungee plugin isn't being used
    to handle bans.  Uses a bit more RAM to keep a list of players, this
    plugin probably isn't going to be used on larger networks anyhow.

--- src/net/gameon365/mc/plugins/bungee/globaljoins/GlobalJoins.java
+++ src/net/gameon365/mc/plugins/bungee/globaljoins/GlobalJoins.java
@@ -1,16 +1,21 @@
 package net.gameon365.mc.plugins.bungee.globaljoins;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 import net.craftminecraft.bungee.bungeeyaml.pluginapi.ConfigurablePlugin;
 
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
-import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.event.ServerConnectedEvent;
+import net.md_5.bungee.api.event.ServerKickEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 public class GlobalJoins extends ConfigurablePlugin implements Listener {
     protected String loginString;
     protected String logoutString;
+    protected Collection<String> playersConnected;
     
     @Override
     public void onEnable()
@@ -18,18 +23,57 @@ public class GlobalJoins extends ConfigurablePlugin implements Listener {
         this.loginString = this.getConfig().getString( "strings.login", "&e%s joined the network." );
         this.logoutString = this.getConfig().getString( "strings.logout", "&e%s left the network." );
         
+        this.playersConnected = new HashSet<>();
+        
         this.getProxy().getPluginManager().registerListener( this, this );
     }
     
-    @EventHandler
-    public void onPostLoginEvent( PostLoginEvent e )
+    @Override
+    public void onDisable()
     {
-        this.getProxy().broadcast( ChatColor.translateAlternateColorCodes( '&', String.format( this.loginString, e.getPlayer().getName() ) ) );
+        for( String p : this.playersConnected )
+        {
+            this.doLogout( p );
+        }
+        
+        this.loginString = null;
+        this.logoutString = null;
+        this.playersConnected = null;
+    }
+    
+    @EventHandler
+    public void onServerConnectedEvent( ServerConnectedEvent e )
+    {
+        this.doLogin( e.getPlayer().getName() );
+    }
+    
+    @EventHandler
+    public void onServerKickEvent( ServerKickEvent e )
+    {
+        this.doLogout( e.getPlayer().getName() );
     }
     
     @EventHandler
     public void onPlayerDisconnectEvent( PlayerDisconnectEvent e )
     {
-        this.getProxy().broadcast( ChatColor.translateAlternateColorCodes( '&', String.format( this.logoutString, e.getPlayer().getName() ) ) );
+        this.doLogout( e.getPlayer().getName() );
+    }
+    
+    public void doLogin( String p )
+    {
+        if( ! this.playersConnected.contains( p ) )
+        {
+            this.getProxy().broadcast( ChatColor.translateAlternateColorCodes( '&', String.format( this.loginString, p ) ) );
+            this.playersConnected.add( p );
+        }
+    }
+    
+    public void doLogout( String p )
+    {
+        if( this.playersConnected.contains( p ) )
+        {
+            this.getProxy().broadcast( ChatColor.translateAlternateColorCodes( '&', String.format( this.logoutString, p ) ) );
+            this.playersConnected.remove( p );
+        }
     }
 }
